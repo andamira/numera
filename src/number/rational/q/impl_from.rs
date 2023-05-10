9c448d9b4103215c0ad1c0282a8c06f9e1d61b2c
joseLuís <joseluis@andamira.net>
2023-05-10 21:49:40 +0200

    add tests for sized rational conversions
    - add tests for `Q`.
    - add missing conversions.
    - small refactoring.

--- src/number/rational/q/impl_from.rs
+++ src/number/rational/q/impl_from.rs
@@ -41,7 +41,7 @@ try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:i+32,64,128];
 try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:i+64,128];
 try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:i+128];
 
-// // from smaller NonZeroU
+// from smaller NonZeroU
 from_integer![nonzero for:Rational+16,num:Z,den:N0z, from:NonZeroU+8];
 from_integer![nonzero for:Rational+32,num:Z,den:N0z, from:NonZeroU+8,16];
 from_integer![nonzero for:Rational+64,num:Z,den:N0z, from:NonZeroU+8,16,32];
@@ -91,38 +91,28 @@ try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:NonZeroInteger+32,
 try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:NonZeroInteger+64,128];
 try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:NonZeroInteger+128];
 
-// from smaller PositiveInteger
-from_integer![primint for:Rational+16,num:Z,den:N0z, from:PositiveInteger+8];
-from_integer![primint for:Rational+32,num:Z,den:N0z, from:PositiveInteger+8,16];
-from_integer![primint for:Rational+64,num:Z,den:N0z, from:PositiveInteger+8,16,32];
-from_integer![primint for:Rational+128,num:Z,den:N0z, from:PositiveInteger+8,16,32,64];
-// try_from bigger or equal sized PositiveInteger
-try_from_integer![primint for:Rational+8,num:Z,den:N0z, from:PositiveInteger+16,32,64,128];
-try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:PositiveInteger+32,64,128];
-try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:PositiveInteger+64,128];
-try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:PositiveInteger+128];
-
 // from smaller NonNegativeInteger
 from_integer![primint for:Rational+16,num:Z,den:N0z, from:NonNegativeInteger+8];
 from_integer![primint for:Rational+32,num:Z,den:N0z, from:NonNegativeInteger+8,16];
 from_integer![primint for:Rational+64,num:Z,den:N0z, from:NonNegativeInteger+8,16,32];
 from_integer![primint for:Rational+128,num:Z,den:N0z, from:NonNegativeInteger+8,16,32,64];
 // try_from bigger or equal sized NonNegativeInteger
-try_from_integer![primint for:Rational+8,num:Z,den:N0z, from:NonNegativeInteger+16,32,64,128];
-try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:NonNegativeInteger+32,64,128];
-try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:NonNegativeInteger+64,128];
-try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:NonNegativeInteger+128];
+try_from_integer![primint for:Rational+8,num:Z,den:N0z, from:NonNegativeInteger+8,16,32,64,128];
+try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:NonNegativeInteger+16,32,64,128];
+try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:NonNegativeInteger+32,64,128];
+try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:NonNegativeInteger+64,128];
 
-// from smaller NegativeInteger
-from_integer![primint for:Rational+16,num:Z,den:N0z, from:NegativeInteger+8];
-from_integer![primint for:Rational+32,num:Z,den:N0z, from:NegativeInteger+8,16];
-from_integer![primint for:Rational+64,num:Z,den:N0z, from:NegativeInteger+8,16,32];
-from_integer![primint for:Rational+128,num:Z,den:N0z, from:NegativeInteger+8,16,32,64];
-// try_from bigger or equal sized NegativeInteger
-try_from_integer![primint for:Rational+8,num:Z,den:N0z, from:NegativeInteger+16,32,64,128];
-try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:NegativeInteger+32,64,128];
-try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:NegativeInteger+64,128];
-try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:NegativeInteger+128];
+// from smaller PositiveInteger
+from_integer![primint for:Rational+16,num:Z,den:N0z, from:PositiveInteger+8];
+from_integer![primint for:Rational+32,num:Z,den:N0z, from:PositiveInteger+8,16];
+from_integer![primint for:Rational+64,num:Z,den:N0z, from:PositiveInteger+8,16,32];
+from_integer![primint for:Rational+128,num:Z,den:N0z, from:PositiveInteger+8,16,32,64];
+// try_from bigger or equal sized PositiveInteger
+try_from_integer![primint for:Rational+8,num:Z,den:N0z, from:PositiveInteger+8,16,32,64,128];
+try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:PositiveInteger+16,32,64,128];
+try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:PositiveInteger+32,64,128];
+try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:PositiveInteger+64,128];
+try_from_integer![primint for:Rational+128,num:Z,den:N0z, from:PositiveInteger+128];
 
 // from smaller NonPositiveInteger
 from_integer![primint for:Rational+16,num:Z,den:N0z, from:NonPositiveInteger+8];
@@ -130,10 +120,25 @@ from_integer![primint for:Rational+32,num:Z,den:N0z, from:NonPositiveInteger+8,1
 from_integer![primint for:Rational+64,num:Z,den:N0z, from:NonPositiveInteger+8,16,32];
 from_integer![primint for:Rational+128,num:Z,den:N0z, from:NonPositiveInteger+8,16,32,64];
 // try_from bigger or equal sized NonPositiveInteger
-try_from_integer![primint for:Rational+8,num:Z,den:N0z, from:NonPositiveInteger+16,32,64,128];
-try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:NonPositiveInteger+32,64,128];
-try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:NonPositiveInteger+64,128];
-try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:NonPositiveInteger+128];
+try_from_integer![primint for:Rational+8,num:Z,den:N0z, from:NonPositiveInteger+8,16,32,64,128];
+try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:NonPositiveInteger+16,32,64,128];
+try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:NonPositiveInteger+32,64,128];
+try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:NonPositiveInteger+64,128];
+try_from_integer![primint for:Rational+128,num:Z,den:N0z, from:NonPositiveInteger+128];
+
+// from smaller NegativeInteger
+from_integer![primint for:Rational+16,num:Z,den:N0z, from:NegativeInteger+8];
+from_integer![primint for:Rational+32,num:Z,den:N0z, from:NegativeInteger+8,16];
+from_integer![primint for:Rational+64,num:Z,den:N0z, from:NegativeInteger+8,16,32];
+from_integer![primint for:Rational+128,num:Z,den:N0z, from:NegativeInteger+8,16,32,64];
+// try_from bigger or equal sized NegativeInteger
+try_from_integer![primint for:Rational+8,num:Z,den:N0z, from:NegativeInteger+8,16,32,64,128];
+try_from_integer![primint for:Rational+16,num:Z,den:N0z, from:NegativeInteger+16,32,64,128];
+try_from_integer![primint for:Rational+32,num:Z,den:N0z, from:NegativeInteger+32,64,128];
+try_from_integer![primint for:Rational+64,num:Z,den:N0z, from:NegativeInteger+64,128];
+try_from_integer![primint for:Rational+128,num:Z,den:N0z, from:NegativeInteger+128];
+
+/* complementary Rational conversions */
 
 // from smaller sized Rational (Self)
 from_rational![for:Rational+16,num:Z,den:N0z, from:Rational+8];
@@ -145,8 +150,6 @@ try_from_rational![for:Rational+8,num:Z,den:N0z, from:Rational+16,32,64,128];
 try_from_rational![for:Rational+16,num:Z,den:N0z, from:Rational+32,64,128];
 try_from_rational![for:Rational+32,num:Z,den:N0z, from:Rational+64,128];
 try_from_rational![for:Rational+64,num:Z,den:N0z, from:Rational+128];
-
-/* complementary Rational conversions */
 
 // // from smaller or equal sized NonZeroRational
 // from_rational![nonzero for:Rational+i+8, from:NonZeroRational+8];
@@ -203,14 +206,96 @@ try_from_rational![for:Rational+64,num:Z,den:N0z, from:Rational+128];
 // try_from_rational![nonzero for:Rational+i+32, from:NegativeRational+64,128];
 // try_from_rational![nonzero for:Rational+i+64, from:NegativeRational+128];
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::all::NumeraResult;
-//
-//     #[test]
-//     fn q_from() -> NumeraResult<()> { // TODO
-//         // let _q5 = Rational8::new((5, 1))?;
-//         Ok(())
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use crate::all::{abbr::*, NumeraResult};
+    use core::num::{NonZeroI16, NonZeroI8, NonZeroU16, NonZeroU8};
+
+    #[test]
+    fn q_from() -> NumeraResult<()> {
+        /* complementary primitive conversions */
+
+        // from smaller u
+        assert_eq![Q16::new(100, 1)?, 100_u8.into()];
+        // try_from bigger or equal sized u
+        assert_eq![Q8::new(100, 1)?, 100_u8.try_into()?];
+        assert_eq![Q8::new(100, 1)?, 100_u16.try_into()?];
+        assert![TryInto::<Q8>::try_into(200_u16).is_err()];
+
+        // from smaller or equal sized i
+        assert_eq![Q8::new(100, 1)?, 100_i8.into()];
+        assert_eq![Q16::new(100, 1)?, 100_i8.into()];
+        // try_from bigger i
+        assert_eq![Q8::new(100, 1)?, 100_i16.try_into()?];
+        assert![TryInto::<Q8>::try_into(200i16).is_err()];
+
+        // from smaller NonZeroU
+        assert_eq![Q16::new(100, 1)?, NonZeroU8::new(100).unwrap().into()];
+        // try_from bigger or equal sized NonZeroU
+        assert_eq![Q8::new(100, 1)?, NonZeroU8::new(100).unwrap().try_into()?];
+        assert_eq![Q8::new(100, 1)?, NonZeroU16::new(100).unwrap().try_into()?];
+        assert![TryInto::<Q8>::try_into(NonZeroU16::new(200).unwrap()).is_err()];
+
+        // from smaller or equal sized NonZeroI
+        assert_eq![Q8::new(100, 1)?, NonZeroI8::new(100).unwrap().into()];
+        assert_eq![Q16::new(100, 1)?, NonZeroI8::new(100).unwrap().into()];
+        // try_from bigger NonZeroI
+        assert_eq![Q8::new(100, 1)?, NonZeroI16::new(100).unwrap().try_into()?];
+        assert![TryInto::<Q8>::try_into(NonZeroI16::new(200).unwrap()).is_err()];
+
+        /* complementary Integer conversions */
+
+        // from smaller or equal sized Integer
+        assert_eq![Q16::new(100, 1)?, Z8::new(100).into()];
+        assert_eq![Q8::new(100, 1)?, Z8::new(100).into()];
+        // try_from bigger Integer
+        assert_eq![Q8::new(100, 1)?, Z16::new(100).try_into()?];
+        assert![TryInto::<Q8>::try_into(Z16::new(200)).is_err()];
+
+        // from smaller or equal sized NonZeroInteger
+        assert_eq![Q16::new(100, 1)?, N0z8::new(100)?.into()];
+        assert_eq![Q8::new(100, 1)?, N0z8::new(100)?.into()];
+        // try_from bigger NonZeroInteger
+        assert_eq![Q8::new(100, 1)?, N0z16::new(100)?.try_into()?];
+        assert![TryInto::<Q8>::try_into(N0z16::new(200)?).is_err()];
+
+        // from smaller NonNegativeInteger
+        assert_eq![Q16::new(100, 1)?, Nnz8::new(100).into()];
+        // from bigger or equal sized NonNegativeInteger
+        assert_eq![Q8::new(100, 1)?, Nnz16::new(100).try_into()?];
+        assert_eq![Q8::new(100, 1)?, Nnz8::new(100).try_into()?];
+        assert![TryInto::<Q8>::try_into(Nnz16::new(200)).is_err()];
+
+        // from smaller PositiveInteger
+        assert_eq![Q16::new(100, 1)?, Pz8::new(100)?.into()];
+        // from bigger or equal sized PositiveInteger
+        assert_eq![Q8::new(100, 1)?, Pz16::new(100)?.try_into()?];
+        assert_eq![Q8::new(100, 1)?, Pz8::new(100)?.try_into()?];
+        assert![TryInto::<Q8>::try_into(Pz16::new(200)?).is_err()];
+
+        // from smaller NonPositiveInteger
+        assert_eq![Q16::new(-100, 1)?, Npz8::new(100).into()];
+        // from bigger or equal sized NonPositiveInteger
+        assert_eq![Q8::new(-100, 1)?, Npz16::new(100).try_into()?];
+        assert_eq![Q8::new(-100, 1)?, Npz8::new(100).try_into()?];
+        assert![TryInto::<Q8>::try_into(Npz16::new(200)).is_err()];
+
+        // from smaller NegativeInteger
+        assert_eq![Q16::new(-100, 1)?, Nz8::new(100)?.into()];
+        // from bigger or equal sized NegativeInteger
+        assert_eq![Q8::new(-100, 1)?, Nz16::new(100)?.try_into()?];
+        assert_eq![Q8::new(-100, 1)?, Nz8::new(100)?.try_into()?];
+        assert![TryInto::<Q8>::try_into(Nz16::new(200)?).is_err()];
+
+        /* complementary Rational conversions */
+
+        // from smaller sized Rational (Self)
+        assert_eq![Q16::new(41, 107)?, Q16::new(41, 107)?.into()];
+        // try_from bigger Rational (Self)
+        assert_eq![Q8::new(41, 107)?, Q16::new(41, 107)?.try_into()?];
+
+        // ...
+
+        Ok(())
+    }
+}
