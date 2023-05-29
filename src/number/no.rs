dee7efbe5bfca032802b68b4a0a6400c515b603e
joseLuís <joseluis@andamira.net>
2023-05-29 11:02:31 +0200

    impl all numeric traits for `()`
    The not-a-number type should implement all specific numeric traits, even
    conflicting ones, in order to be able to be used whenever any kind of
    number is requested, to be an indicator of its absence.

--- src/number/no.rs
+++ src/number/no.rs
@@ -1,26 +1,33 @@
 //
-//!
+//! Not a number.
 //
+// TOC
+//
+// - Number
+// - Bound
+// - Count
+// - Ident
+// - Sign
 
-use crate::{
-    error::NumeraResult as Result,
-    number::traits::{Bound, Count, Ident, Number, Sign},
-};
+use crate::{error::NumeraResult, number::traits::*};
+
+/* Number */
 
 /// This implementation allows to use the unit type for representing
 /// the absence of a number, where a number is expected.
 #[rustfmt::skip]
 impl Number for () {
     type Parts = ();
-
     #[inline]
-    fn from_parts(value: ()) -> Result<Self> { Ok(value) }
-
+    fn from_parts(value: ()) -> NumeraResult<Self> { Ok(value) }
     #[inline]
     #[cfg(not(feature = "safe"))]
     #[cfg_attr(feature = "nightly", doc(cfg(feature = "unsafe")))]
     unsafe fn from_parts_unchecked(value: ()) -> Self { value }
 }
+
+/* Bound */
+
 #[rustfmt::skip]
 impl Bound for () {
     #[inline]
@@ -28,16 +35,45 @@ impl Bound for () {
     #[inline]
     fn is_upper_bounded(&self) -> bool { true }
     #[inline]
-    fn lower_bound(&self) -> Option<Self> where Self: Sized { Some(()) }
+    fn lower_bound(&self) -> Option<Self> { Some(()) }
     #[inline]
-    fn upper_bound(&self) -> Option<Self> where Self: Sized { Some(()) }
+    fn upper_bound(&self) -> Option<Self> { Some(()) }
 }
+// these imply Bounded:
+impl LowerBounded for () {
+    #[inline]
+    fn new_min() -> Self {}
+}
+impl UpperBounded for () {
+    #[inline]
+    fn new_max() -> Self {}
+}
+// these imply ConstBounded:
+#[rustfmt::skip]
+impl ConstLowerBounded for () { const MIN: Self = (); }
+#[rustfmt::skip]
+impl ConstUpperBounded for () { const MAX: Self = (); }
+// these imply NonBounded:
+impl NonLowerBounded for () {}
+impl NonUpperBounded for () {}
+
+/* Count */
 
 #[rustfmt::skip]
 impl Count for () {
     #[inline]
     fn is_countable(&self) -> bool { false }
 }
+#[rustfmt::skip]
+impl Countable for () {
+    #[inline]
+    fn next(&self) -> NumeraResult<Self> { Ok(()) }
+    #[inline]
+    fn previous(&self) -> NumeraResult<Self> { Ok(()) }
+}
+impl Uncountable for () {}
+
+/* Ident */
 
 #[rustfmt::skip]
 impl Ident for () {
@@ -54,6 +90,27 @@ impl Ident for () {
     #[inline]
     fn is_neg_one(&self) -> bool { false }
 }
+impl NonZero for () {}
+#[rustfmt::skip]
+impl ConstOne for () { const ONE: Self = (); }
+#[rustfmt::skip]
+impl ConstZero for () { const ZERO: Self = (); }
+#[rustfmt::skip]
+impl ConstNegOne for () { const NEG_ONE: Self = (); }
+impl One for () {
+    #[inline]
+    fn new_one() -> Self {}
+}
+impl Zero for () {
+    #[inline]
+    fn new_zero() -> Self {}
+}
+impl NegOne for () {
+    #[inline]
+    fn new_neg_one() -> Self {}
+}
+
+/* Sign */
 
 #[rustfmt::skip]
 impl Sign for () {
@@ -65,4 +122,15 @@ impl Sign for () {
     fn is_positive(&self) -> bool { false }
     #[inline]
     fn is_negative(&self) -> bool { false }
+}
+// these imply Signed:
+impl Positive for () {}
+impl Negative for () {}
+// these imply Unsigned, NonOne, NonNegOne:
+impl NonNegative for () {}
+#[rustfmt::skip]
+impl NonPositive for () {
+    type Parts = ();
+    #[inline]
+    fn new_neg(value: Self::Parts) -> NumeraResult<Self::Parts> { Ok(value) }
 }
