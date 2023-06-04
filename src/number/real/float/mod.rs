070ca795c6bec07dd7948e7d6335a7c51daf5089
joseLuís <joseluis@andamira.net>
2023-06-04 22:39:17 +0200

    improve floating point types
    - new feature: `float`.
    - new types: `BFloat16`, `Float16`.
    - new placeholder type `Float128`.
    - refactor `float` module.

--- src/number/real/float/mod.rs
+++ src/number/real/float/mod.rs
@@ -13,7 +13,20 @@ mod sized;
 
 pub use sized::{Float32, Float64};
 
+#[cfg(feature = "twofloat")]
+pub use sized::Float128;
+#[cfg(feature = "half")]
+pub use sized::{BFloat16, Float16};
+
 use crate::number::macros::define_abbreviations;
-// define_abbreviations![F, Float, 32, 64]; // TEMP
+
+// define_abbreviations![F, Float, 32, 64]; TODO: needs family
 define_abbreviations![sized F, Float, 32];
 define_abbreviations![sized F, Float, 64];
+
+#[cfg(feature = "half")]
+define_abbreviations![sized F, Float, 16];
+#[cfg(feature = "half")]
+define_abbreviations![sized Bf, BFloat, 16]; // better name?
+#[cfg(feature = "twofloat")]
+define_abbreviations![sized F, Float, 128];
---
+++ src/number/real/float/sized/f128.rs
@@ -0,0 +1,10 @@
+// numera::number::real::float::f::sized::external_128
+//
+//!
+//
+// TODO
+
+use twofloat::TwoFloat;
+
+// TEMP
+pub struct Float128;
